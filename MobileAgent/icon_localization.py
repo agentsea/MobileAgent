f93cf77472fb31e641af1d0b0992bb2a69c9ad33
Patrick Barker <patrickbarkerco@gmail.com>
2024-02-29 08:13:22 -0800

    more deps

--- MobileAgent/icon_localization.py
+++ MobileAgent/icon_localization.py
@@ -1,11 +1,29 @@
 import torch
 import numpy as np
-from MobileAgent.crop import calculate_size, calculate_iou
 from PIL import Image
 import groundingdino.datasets.transforms as T
 from groundingdino.models import build_model
 from groundingdino.util.slconfig import SLConfig
 from groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+
+
+def calculate_size(box):
+    return (box[2] - box[0]) * (box[3] - box[1])
+
+
+def calculate_iou(box1, box2):
+    xA = max(box1[0], box2[0])
+    yA = max(box1[1], box2[1])
+    xB = min(box1[2], box2[2])
+    yB = min(box1[3], box2[3])
+
+    interArea = max(0, xB - xA) * max(0, yB - yA)
+    box1Area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2Area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    unionArea = box1Area + box2Area - interArea
+    iou = interArea / unionArea
+
+    return iou
 
 
 def transform_image(image_pil):
@@ -26,13 +44,17 @@ def load_model(model_config_path, model_checkpoint_path, device):
     args.device = device
     model = build_model(args)
     checkpoint = torch.load(model_checkpoint_path, map_location="cpu")
-    load_res = model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
+    load_res = model.load_state_dict(
+        clean_state_dict(checkpoint["model"]), strict=False
+    )
     print(load_res)
     _ = model.eval()
     return model
 
 
-def get_grounding_output(model, image, caption, box_threshold, text_threshold, with_logits=True):
+def get_grounding_output(
+    model, image, caption, box_threshold, text_threshold, with_logits=True
+):
     caption = caption.lower()
     caption = caption.strip()
     if not caption.endswith("."):
@@ -57,7 +79,9 @@ def get_grounding_output(model, image, caption, box_threshold, text_threshold, w
     pred_phrases = []
     scores = []
     for logit, box in zip(logits_filt, boxes_filt):
-        pred_phrase = get_phrases_from_posmap(logit > text_threshold, tokenized, tokenlizer)
+        pred_phrase = get_phrases_from_posmap(
+            logit > text_threshold, tokenized, tokenlizer
+        )
         if with_logits:
             pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
         else:
@@ -71,10 +95,10 @@ def remove_boxes(boxes_filt, size, iou_threshold=0.5):
     boxes_to_remove = set()
 
     for i in range(len(boxes_filt)):
-        if calculate_size(boxes_filt[i]) > 0.05*size[0]*size[1]:
+        if calculate_size(boxes_filt[i]) > 0.05 * size[0] * size[1]:
             boxes_to_remove.add(i)
         for j in range(len(boxes_filt)):
-            if calculate_size(boxes_filt[j]) > 0.05*size[0]*size[1]:
+            if calculate_size(boxes_filt[j]) > 0.05 * size[0] * size[1]:
                 boxes_to_remove.add(j)
             if i == j:
                 continue
@@ -84,21 +108,33 @@ def remove_boxes(boxes_filt, size, iou_threshold=0.5):
             if iou >= iou_threshold:
                 boxes_to_remove.add(j)
 
-    boxes_filt = [box for idx, box in enumerate(boxes_filt) if idx not in boxes_to_remove]
-    
+    boxes_filt = [
+        box for idx, box in enumerate(boxes_filt) if idx not in boxes_to_remove
+    ]
+
     return boxes_filt
 
 
-def det(input_image, text_prompt, groundingdino_model, box_threshold=0.05, text_threshold=0.5):
+def det(
+    input_image,
+    text_prompt,
+    groundingdino_model,
+    box_threshold=0.05,
+    text_threshold=0.5,
+):
     image = Image.open(input_image)
     size = image.size
 
     image_pil = image.convert("RGB")
     image = np.array(image_pil)
-    
+
     transformed_image = transform_image(image_pil)
     boxes_filt, scores, pred_phrases = get_grounding_output(
-        groundingdino_model, transformed_image, text_prompt, box_threshold, text_threshold
+        groundingdino_model,
+        transformed_image,
+        text_prompt,
+        box_threshold,
+        text_threshold,
     )
 
     H, W = size[1], size[0]
@@ -108,11 +144,25 @@ def det(input_image, text_prompt, groundingdino_model, box_threshold=0.05, text_
         boxes_filt[i][2:] += boxes_filt[i][:2]
 
     boxes_filt = boxes_filt.cpu().int().tolist()
-    filtered_boxes = remove_boxes(boxes_filt, size)#[:9]
+    filtered_boxes = remove_boxes(boxes_filt, size)  # [:9]
     coordinate = []
     image_data = []
     for box in filtered_boxes:
-        image_data.append([max(0, box[0]-10), max(0, box[1]-10), min(box[2]+10, size[0]), min(box[3]+10, size[1])])
-        coordinate.append([max(0, box[0]-25), max(0, box[1]-25), min(box[2]+25, size[0]), min(box[3]+25, size[1])])
+        image_data.append(
+            [
+                max(0, box[0] - 10),
+                max(0, box[1] - 10),
+                min(box[2] + 10, size[0]),
+                min(box[3] + 10, size[1]),
+            ]
+        )
+        coordinate.append(
+            [
+                max(0, box[0] - 25),
+                max(0, box[1] - 25),
+                min(box[2] + 25, size[0]),
+                min(box[3] + 25, size[1]),
+            ]
+        )
 
     return image_data, coordinate
